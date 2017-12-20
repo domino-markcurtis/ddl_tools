93e1cf1c41bb54e01fef0498c23c558a7f5aade6
domino-markcurtis <34025557+domino-markcurtis@users.noreply.github.com>
2017-12-20 18:17:10 +0000

    Create diag.sh

---
+++ diag.sh
@@ -0,0 +1,19 @@
+# Grab diags from server
+
+# Init any vars
+DATE=$(date --iso-8601=seconds)
+
+# Output files
+OUT_SJK_CPU=$(hostname)-$DATE-sjk-cpu.out
+OUT_SJK_ALLOC=$(hostname)-$DATE-sjk-alloc.out
+OUT_JSTACK=$(hostname)-$DATE-jstack.out
+
+# Get process id of frontend java
+PID=$(sudo -u domino jps | grep "ProdServerStart" | awk '{print $1}')
+
+# Get sjk output
+sudo -u domino java -jar sjk-plus-0.8.1.jar ttop -p $PID -o CPU -n 20 >> $OUT_SJK_CPU &
+sudo -u domino java -jar sjk-plus-0.8.1.jar ttop -p $PID -o ALLOC -n 20 >> $OUT_SJK_ALLOC &
+
+# Get thread dumps
+sudo -u domino jstack -l $PID >> $OUT_JSTACK
